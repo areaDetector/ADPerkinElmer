5376efbac9e22d44372fa747180a7f1dac9e01c8
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-08-11 03:56:28 +0000

    Changes in attribute API
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@9221 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/perkinElmerSrc/PerkinElmer.cpp
+++ ADApp/perkinElmerSrc/PerkinElmer.cpp
@@ -617,7 +617,7 @@ const char *functionName = "frameCallback";
     pImage->timeStamp = startTime.secPastEpoch + startTime.nsec / 1.e9;
 
     /* Get any attributes that have been defined for this driver */        
-    this->getAttributes(pImage);
+    this->getAttributes(pImage->pAttributes);
 
     /* Call the NDArray callback */
     /* Must release the lock here, or we can get into a deadlock, because we can
