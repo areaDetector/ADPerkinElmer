4166094717a84da0503d880c612a4f9af8ba820b
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-08-23 02:56:41 +0000

    Obsolete
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@9371 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocPerkinElmer/ioc_start.bat
+++
@@ -1,5 +0,0 @@
-#set PATH=.;c:\Progra~1\Java\jdk1.6.0_10\jre\bin\client;c:\Progra~1\Java\jdk1.6.0_10\bin;c:\Progra~1\Java\jdk1.6.0_10\jre\bin
-set path=%path%;C:\XIS\SDK
-set path=%path%;C:\epics\base-3.14.10\bin\win32-x86-debug
-set EPICS_CA_ADDR_LIST=164.54.62.11
-C:\epics\synApps_5_4_1\support\areaDetector\1-5beta\bin\win32-x86-debug\PerkinElmerApp.exe st.cmd.win32
