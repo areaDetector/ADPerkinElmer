ee8a2e3dbc06bcc0de4454e5352f6772a9dc4bec
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-04-09 20:19:22 +0000

    Added XSIZE and YSIZE environment variables for profiles
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@10611 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocPerkinElmer/st.cmd
+++ iocBoot/iocPerkinElmer/st.cmd
@@ -8,6 +8,8 @@ PerkinElmerApp_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("PREFIX", "13PE1:")
 epicsEnvSet("PORT",   "PEDET1")
 epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("XSIZE",  "1024")
+epicsEnvSet("YSIZE",  "1024")
 
 # Create a PerkinElmer driver
 # PerkinElmerConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType, int maxBuffers, size_t maxMemory, int priority, int stackSize)
@@ -21,7 +23,7 @@ epicsEnvSet("QSIZE",  "20")
 #    6 = NDFloat32
 #    7 = NDFloat64
 #
-PerkinElmerConfig("$(PORT)", 1024, 1024, 3, 100, 200000000)
+PerkinElmerConfig("$(PORT)", $(XSIZE), $(YSIZE), 3, 100, 200000000)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",     "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/PerkinElmer.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
