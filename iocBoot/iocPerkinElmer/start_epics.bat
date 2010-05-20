d5fbc87fdeb796b9fef5c3ef0e6f4d5942e1359b
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-05-20 21:22:25 +0000

    Removed unneeded macro parameters
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@11047 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocPerkinElmer/start_epics.bat
+++ iocBoot/iocPerkinElmer/start_epics.bat
@@ -1,4 +1,3 @@
-set path=%path%;C:\XIS\SDK
-start medm -x -macro "P=JPHPE1:, R=cam1:, I=image1:, ROI=ROI1:, NETCDF=netCDF1:, TIFF=TIFF1:, JPEG=JPEG1:, NEXUS=Nexus1:" PerkinElmer.adl
+start medm -x -macro "P=JPHPE1:, R=cam1:" PerkinElmer.adl
 ..\..\bin\win32-x86\PerkinElmerApp st.cmd
 
