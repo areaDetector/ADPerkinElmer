b1dadd445bf08b8884cfaff81a353aa4adb325a8
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2011-12-02 04:13:47 +0000

    Changed syntax of PerkinElmerConfig
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14219 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocPerkinElmer/st.cmd
+++ iocBoot/iocPerkinElmer/st.cmd
@@ -8,23 +8,17 @@ PerkinElmerApp_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("PREFIX", "13PE1:")
 epicsEnvSet("PORT",   "PEDET1")
 epicsEnvSet("QSIZE",  "20")
-epicsEnvSet("XSIZE",  "1024")
-epicsEnvSet("YSIZE",  "1024")
+epicsEnvSet("XSIZE",  "2048")
+epicsEnvSet("YSIZE",  "2048")
 epicsEnvSet("NCHANS", "2048")
 
 # Create a PerkinElmer driver
-# PerkinElmerConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType, int maxBuffers, size_t maxMemory, int priority, int stackSize)
-# DataTypes:
-#    0 = NDInt8
-#    1 = NDUInt8
-#    2 = NDInt16
-#    3 = NDUInt16
-#    4 = NDInt32
-#    5 = NDUInt32
-#    6 = NDFloat32
-#    7 = NDFloat64
-#
-PerkinElmerConfig("$(PORT)", $(XSIZE), $(YSIZE), 3, 100, 200000000)
+# PerkinElmerConfig(const char *portName, maxBuffers, size_t maxMemory, int priority, int stackSize)
+PerkinElmerConfig("$(PORT)", 100, 200000000)
+
+asynSetTraceIOMask($(PORT), 0, 2)
+#asynSetTraceMask($(PORT),0,0xff)
+
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",     "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/PerkinElmer.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
@@ -36,10 +30,9 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=i
 # Load all other plugins using commonPlugins.cmd
 < ../commonPlugins.cmd
 
-#asynSetTraceIOMask("$(PORT)",0,2)
-#asynSetTraceMask("$(PORT)",0,255)
-
 iocInit()
 
 # save things every thirty seconds
 create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+
+#asynSetTraceMask($(PORT),0,0xff)
