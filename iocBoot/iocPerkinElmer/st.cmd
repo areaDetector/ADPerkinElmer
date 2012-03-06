ab18f5909f849b476519560665966c16b51ae073
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2012-03-06 00:40:02 +0000

    Changes to support GigE detectors
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14576 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocPerkinElmer/st.cmd
+++ iocBoot/iocPerkinElmer/st.cmd
@@ -13,8 +13,23 @@ epicsEnvSet("YSIZE",  "2048")
 epicsEnvSet("NCHANS", "2048")
 
 # Create a PerkinElmer driver
-# PerkinElmerConfig(const char *portName, maxBuffers, size_t maxMemory, int priority, int stackSize)
-PerkinElmerConfig("$(PORT)", 100, 200000000)
+# PerkinElmerConfig(const char *portName, IDType, IDValue, maxBuffers, size_t maxMemory, int priority, int stackSize)
+# IDType = 0 Frame grabber card, IDValue="", use first 
+#        = 1 GigE detector by IP address (e.g. 164.54.160.21)
+#        = 2 GigE detector by MAC address (e.g. 00005b032e6b, must be lower-case letters)
+#        = 3 GigE detector by IP name (e.g. gse-pe1.cars.aps.anl.gov)
+
+# This is for the first PCI/PCIExpress frame grabber detector in the system
+#PerkinElmerConfig("$(PORT)", 0, "", 100, 200000000, 0, 0)
+
+# This is for a GigE frame grabber at IP address 164.54.160.204
+#PerkinElmerConfig("$(PORT)", 1, 164.54.160.204, 100, 200000000, 0, 0)
+
+# This is for a GigE frame grabber at MAC address 00005b032e6b
+PerkinElmerConfig("$(PORT)", 2, 00005b032e6b, 100, 200000000, 0, 0)
+
+# This is for a GigE frame grabber at IP  name dhcp5.cars.aps.anl.gov
+#PerkinElmerConfig("$(PORT)", 3, DHCP5.cars.aps.anl.gov, 100, 200000000, 0, 0)
 
 asynSetTraceIOMask($(PORT), 0, 2)
 #asynSetTraceMask($(PORT),0,0xff)
