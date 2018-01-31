e9daeeec23830e09aac9c1b2c4fb82c6e757fee3
Mark Rivers <rivers@cars.uchicago.edu>
2018-01-31 11:17:59 -0600

    Updated DRIVER_REVISION to 7

--- perkinElmerApp/src/PerkinElmer.cpp
+++ perkinElmerApp/src/PerkinElmer.cpp
@@ -44,7 +44,7 @@
 #include "PerkinElmer.h"
 
 #define DRIVER_VERSION      2
-#define DRIVER_REVISION     6
+#define DRIVER_REVISION     7
 #define DRIVER_MODIFICATION 0
 
 // Forward function definitions
